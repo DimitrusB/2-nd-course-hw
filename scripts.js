e8f82d1cd281fe6ed9b0d116d233ca7a70652da6
DimitrusB <dimitrusboss@mail.ru>
2023-03-27 22:13:56 +0300

    +js

---
+++ scripts.js
@@ -0,0 +1,80 @@
+// // --------------------------------------1
+// let password = 'пароль';
+// let reqPass = String(prompt('Введите пароль'));
+// reqPass = reqPass.toLocaleLowerCase();
+// if (reqPass === password) {
+//     console.log("Пароль введен верно");    
+// } else {
+//     console.log("Пароль введен неправильно");
+// }
+// // --------------------------------------2
+// let c = Number(prompt('Введите число'));
+// if (c>0 && c<10) {
+//     console.log('Верно');
+// } else {
+//     console.log('Неверно');
+// }
+// // --------------------------------------3
+// let d = Number(prompt('Введите число'));
+// let e = Number(prompt('Введите число'));
+// if (d > 100 || e > 100) {
+//     console.log('Верно');
+// } else {
+//     console.log('Неверно');   
+// }
+// // --------------------------------------4
+// let a = '2';
+// let b = '3';
+// alert (parseInt(a) + parseInt(b));
+// // --------------------------------------5
+// let monthNumber = Number(prompt('Введите номер месяца в году, для определения сезона'));
+// switch (monthNumber) {
+//     case 1:
+//         console.log('Сейчас зима');
+//         break;
+//     case 2:
+//         console.log('Сейчас зима');
+//         break;
+//         case 1:
+//     case 3:
+//         console.log('Сейчас весна');
+//         break;
+//     case 4:
+//         console.log('Сейчас весна');
+//         break;
+//     case 5:
+//         console.log('Сейчас весна');
+//         break;
+//     case 6:
+//         console.log('Сейчас лето');
+//         break;
+//     case 7:
+//         console.log('Сейчас лето');
+//         break;
+//     case 8:
+//         console.log('Сейчас лето');
+//         break;
+//     case 9:
+//         console.log('Сейчас осень');
+//         break;
+//     case 10:
+//         console.log('Сейчас осень');
+//         break;
+//     case 11:
+//         console.log('Сейчас осень');
+//         break;
+//     case 12:
+//         console.log('Сейчас зима');
+//         break;
+//     default:
+//         console.log('Ошибка: такого месяца нет!!!');
+//         break;
+// }
+// // --------------------------------------6 DOP
+// let num = Number(prompt('Пожалуйста, введите любое число'));
+// let result = num % 2;
+// if (result != 0) {
+//     alert('Число нечетное')
+// } else {
+//     alert('Число четное')    
+// }
