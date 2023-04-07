94601542f99959fed0059865284c20f7a760b991
DimitrusB <dimitrusboss@mail.ru>
2023-04-07 22:20:04 +0300

    10

--- scripthw-6.js
+++ scripthw-6.js
@@ -65,13 +65,15 @@
 // }
 // console.log(arr);
 // console.log(arrAll);
-// =====================================#10!!
+// =====================================#10
 // function red(){
-//     let arr = [1,3,5,3,2,5,6];
-// const sum=arr.reduce((a,b) => a+b);
-// console.log(sum);
+//     let arr = [1,3,5,4,2,5,6];
+//     for (let i = 0; i < arr.length-1; i++) {
+//         sum= arr[i]+arr[i+1];
+//         console.log(`сумма текущего и следующего элемента массива равна ${sum}`);
+//     }
 // }
-//     console.log(red);
+// red();
 // =====================================#11
 // let arr = [1, 2, 3, 4, 5];
 // function square(){
