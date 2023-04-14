e4ed488cde76cc1dbc441850b87da774ea09f124
DimitrusB <dimitrusboss@mail.ru>
2023-04-14 23:02:04 +0300

    2

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
 // ========================================#1
-function mult(){
-    let res =1;
-        for (let i = 0; i < arr.length; i++) {
-            res *= arr[i] ;
-        }
-        console.log(`произведение чисел массива равно ${res}`);
-}
-function sum(){
-    let res =0;
-    for (let i = 0; i < arr.length; i++) {
-        res += arr[i] ;
-    }
-    console.log(`сумма чисел массива равна ${res}`);
-}
+// function mult(){
+//     let res =1;
+//         for (let i = 0; i < arr.length; i++) {
+//             res *= arr[i] ;
+//         }
+//         console.log(`произведение чисел массива равно ${res}`);
+// }
+// function sum(){
+//     let res =0;
+//     for (let i = 0; i < arr.length; i++) {
+//         res += arr[i] ;
+//     }
+//     console.log(`сумма чисел массива равна ${res}`);
+// }
 
-function getResult(arr, callback){
-callback();
-}
-let arr = [3, 4, 1, 9];
-getResult(arr, mult);
-getResult(arr, sum);
+// function getResult(arr, callback){
+// callback();
+// }
+// let arr = [3, 4, 1, 9];
+// getResult(arr, mult);
+// getResult(arr, sum);
 // ========================================#2
+// const users = [
+// 	{name: 'Jon', age: 22},
+// 	{name: 'Richard', age: 18},
+// 	{name: 'Anton', age: 32},
+// 	{name: 'Lida', age: 23},
+// 	{name: 'Bob', age: 44}
+// ];
+// users.sort(function (a, b) {
+
+//     return a.age - b.age;
+// });
+// console.log(users);
+
