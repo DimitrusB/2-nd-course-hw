523857b223c81ff1ba8ed96f23865b97837228bc
DimitrusB <dimitrusboss@mail.ru>
2023-04-14 22:26:23 +0300

    2

--- script.js
+++ script.js
@@ -1,29 +1,23 @@
 // ========================================#1
-// const num = [3, 4, 1, 9];
 function mult(){
-
     let res =1;
         for (let i = 0; i < arr.length; i++) {
-            res *= Array[i] ;
+            res *= arr[i] ;
         }
         console.log(`произведение чисел массива равно ${res}`);
 }
-
 function sum(){
     let res =0;
     for (let i = 0; i < arr.length; i++) {
-        res += Array[i] ;
+        res += arr[i] ;
     }
     console.log(`сумма чисел массива равна ${res}`);
 }
-// sum([3, 4, 1, 9]);
 
-let arr =[];
 function getResult(arr, callback){
-
 callback();
 }
-
-
-getResult([3, 4, 1, 9], mult);
-getResult([3, 4, 1, 9], sum);
+let arr = [3, 4, 1, 9];
+getResult(arr, mult);
+getResult(arr, sum);
+// ========================================#2
